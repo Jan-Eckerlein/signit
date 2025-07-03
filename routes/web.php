235ef1e47551f689aa40ce90241e5c3a740e362b
Jan Eckerlein <eckerlein.jan@gmail.com>
2025-07-03 20:23:17 +0200

    Remove mock api routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-use App\Http\Requests\SignDocumentRequest;
 use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix' => 'api'], function () {
-    Route::get('/test-me', function () {
-        return response()->json(['message' => 'Hello from Laravel!']);
-    });
-
-    Route::post('/sign-document', function (SignDocumentRequest $request) {
-        $validated = $request->validated();
-        return response()->json(['message' => 'Hello from Laravel!', 'data' => $validated]);
-    });
-});
-
 
 Route::get('/editor/{vue_capture?}', function () {
     return view('welcome');
